4db32b3c2a4cd468047a9b18df00be3ac1649672
spezialissimo <davide.speziali@proton.me>
2024-05-19 21:00:46 +0200

    :zzz: Remove useless annotation

--- app/app/src/main/java/com/example/gnammy/MainActivity.kt
+++ app/app/src/main/java/com/example/gnammy/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -17,7 +16,6 @@ import com.example.gnammy.ui.composables.TopBar
 import com.example.gnammy.ui.theme.GnammyTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
