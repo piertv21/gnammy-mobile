b08b69e86b4f21df5c45232739443b8372af9682
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2024-05-21 13:13:10 +0200

    Add Login screen

--- app/app/src/main/java/com/example/gnammy/ui/screens/login/LoginScreen.kt
+++ app/app/src/main/java/com/example/gnammy/ui/screens/login/LoginScreen.kt
@@ -1,2 +1,75 @@
 package com.example.gnammy.ui.screens.login
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable
+fun LoginScreen(navHostController: NavHostController) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(60.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Accedi", fontWeight = FontWeight.Bold, fontSize = 30.sp)
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = { /* Focus next input field */ })
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = { /* Perform login action */ })
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            "Non hai un'account?",
+            color = Color.Gray
+        )
+
+        Button(
+            onClick = {
+                // Handle registration action
+            }
+        ) {
+            Text("Registrati")
+        }
+    }
+}
