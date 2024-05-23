c8cf7b951f7ff0744c07dc73a0bffdc42f66efbe
spezialissimo <davide.speziali@proton.me>
2024-05-23 12:24:09 +0200

    🤙️ Add notification pills

--- app/app/src/main/java/com/example/gnammy/ui/composables/NotificationPill.kt
+++ app/app/src/main/java/com/example/gnammy/ui/composables/NotificationPill.kt
@@ -1,36 +1,131 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+@file:Suppress("DEPRECATION")
+
 package com.example.gnammy.ui.composables
 
+import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissState
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.Icon
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.rememberDismissState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-@Preview
+fun DismissBackground(dismissState: DismissState) {
+    val direction = dismissState.dismissDirection
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(6.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        if (direction == DismissDirection.StartToEnd) {
+            Icon(
+                Icons.Default.Delete,
+                contentDescription = "delete",
+                modifier = Modifier
+                    .aspectRatio(1f)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.error)
+                    .padding(15.dp)
+            )
+        }
+        Spacer(modifier = Modifier)
+        if (direction == DismissDirection.EndToStart) {
+            Icon(
+                Icons.Default.Delete,
+                contentDescription = "delete",
+                modifier = Modifier
+                    .aspectRatio(1f)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.error)
+                    .padding(15.dp)
+            )
+        }
+    }
+}
+
+@Composable
 fun NotificationPill() {
-    Row (
+    val context = LocalContext.current
+    var show by remember { mutableStateOf(true) }
+    val dismissState = rememberDismissState(
+        confirmStateChange = {
+            if (it == DismissValue.DismissedToStart || it == DismissValue.DismissedToEnd) {
+                show = false
+                true
+            } else false
+        }
+    )
+    AnimatedVisibility(
+        show, exit = fadeOut(spring())
+    ) {
+        SwipeToDismiss(
+            state = dismissState,
+            modifier = Modifier,
+            background = {
+                DismissBackground(dismissState)
+            },
+            dismissContent = {
+                Pill()
+            },
+            dismissThresholds = {
+                FractionalThreshold(0.5f)
+            }
+        )
+    }
+
+    LaunchedEffect(show) {
+        if (!show) {
+            Toast.makeText(context, "Item removed", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+@Composable
+fun Pill(){
+    Box (
         modifier = Modifier
             .aspectRatio(5f / 1f)
+            .padding(5.dp)
             .fillMaxWidth()
             .clip(shape = androidx.compose.foundation.shape.CircleShape)
-            .background(color = MaterialTheme.colorScheme.inversePrimary)
+            .background(color = MaterialTheme.colorScheme.primary)
     ){
         ImageWithPlaceholder(
             uri = null,
@@ -44,21 +139,15 @@ fun NotificationPill() {
                 .border(2.dp, MaterialTheme.colorScheme.background, CircleShape)
         )
 
-        Spacer(modifier = Modifier.width(10.dp))
-
         Box(
-            contentAlignment = Alignment.Center,
             modifier = Modifier.fillMaxSize()
         ) {
             Text(
                 text = "Paolo ha salvato il tuo gnam!",
                 color = MaterialTheme.colorScheme.background,
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.Center)
             )
         }
-
-        Spacer(modifier = Modifier.width(10.dp))
-
-
     }
 }
--- app/app/src/main/java/com/example/gnammy/ui/screens/notification/NotificationScreen.kt
+++ app/app/src/main/java/com/example/gnammy/ui/screens/notification/NotificationScreen.kt
@@ -2,12 +2,16 @@ package com.example.gnammy.ui.screens.notification
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.example.gnammy.ui.composables.NotificationPill
 
 @Composable
 fun NotificationScreen(navController: NavHostController, modifier: Modifier) {
@@ -17,6 +21,10 @@ fun NotificationScreen(navController: NavHostController, modifier: Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Notification Page")
+        LazyColumn {
+            items(10) {
+                NotificationPill()
+            }
+        }
     }
 }
