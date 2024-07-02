1c5a905c5abd2e78b0d07e79635c7f45e7f19579
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2024-07-02 15:38:19 +0200

    Removed unused fun

--- app/app/src/main/java/com/example/gnammy/utils/Image.kt
+++ app/app/src/main/java/com/example/gnammy/utils/Image.kt
@@ -29,28 +29,6 @@ fun uriToBitmap(imageUri: Uri, contentResolver: ContentResolver): Bitmap {
     return bitmap
 }
 
-fun saveImageToStorage(
-    imageUri: Uri,
-    contentResolver: ContentResolver,
-    name: String = "IMG_${SystemClock.uptimeMillis()}"
-): Uri {
-    val bitmap = uriToBitmap(imageUri, contentResolver)
-
-    val values = ContentValues()
-    values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-    values.put(MediaStore.Images.Media.DISPLAY_NAME, name)
-
-    val savedImageUri =
-        contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-    val outputStream = savedImageUri?.let { contentResolver.openOutputStream(it) }
-        ?: throw FileNotFoundException()
-
-    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-    outputStream.close()
-
-    return savedImageUri
-}
-
 fun getFileFromUri(context: Context, uri: Uri): File? {
     val inputStream = context.contentResolver.openInputStream(uri) ?: return null
     val tempFile = File.createTempFile("temp_", ".jpg", context.cacheDir)
