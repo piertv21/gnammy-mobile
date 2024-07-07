4a8111ee0fc425f5c8d1cf63f6389acadc997577
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2024-07-07 16:26:43 +0200

    Add DataStoreHelper for future transition

---
+++ app/app/src/main/java/com/example/gnammy/utils/DataStoreHelper.kt
@@ -0,0 +1,76 @@
+package com.example.gnammy.utils
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class DataStoreHelper(private val context: Context, private val dataStoreName: String) {
+
+    private val Context.dataStore by preferencesDataStore(name = dataStoreName)
+
+    suspend fun putStringValue(key: String, value: String) {
+        val dataStoreKey = stringPreferencesKey(key)
+        context.dataStore.edit { preferences ->
+            preferences[dataStoreKey] = value
+        }
+    }
+
+    suspend fun putIntValue(key: String, value: Int) {
+        val dataStoreKey = intPreferencesKey(key)
+        context.dataStore.edit { preferences ->
+            preferences[dataStoreKey] = value
+        }
+    }
+
+    suspend fun removeValue(key: String) {
+        val stringKey = stringPreferencesKey(key)
+        val intKey = intPreferencesKey(key)
+        context.dataStore.edit { preferences ->
+            preferences.remove(stringKey)
+            preferences.remove(intKey)
+        }
+    }
+
+    fun getStringValue(key: String): Flow<String?> {
+        val dataStoreKey = stringPreferencesKey(key)
+        return context.dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }
+            .map { preferences ->
+                preferences[dataStoreKey]
+            }
+    }
+
+    fun getIntValue(key: String): Flow<Int?> {
+        val dataStoreKey = intPreferencesKey(key)
+        return context.dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }
+            .map { preferences ->
+                preferences[dataStoreKey]
+            }
+    }
+
+    suspend fun clearAll() {
+        context.dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
+}
