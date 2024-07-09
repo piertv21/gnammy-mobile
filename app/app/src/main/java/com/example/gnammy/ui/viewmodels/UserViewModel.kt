84e43e41814f26df4715789188c68523177ad817
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2024-07-09 08:53:01 +0200

    Fix a bug where loggedUserId where not actually updated after login

--- app/app/src/main/java/com/example/gnammy/ui/viewmodels/UserViewModel.kt
+++ app/app/src/main/java/com/example/gnammy/ui/viewmodels/UserViewModel.kt
@@ -88,6 +88,7 @@ class UserViewModel(private val repository: UserRepository) : ViewModel() {
     }
 
     suspend fun logout() {
+        loggedUserId = "NOT SET"
         repository.clearData()
     }
 }
